739d1a8e0199ff93ea6c8a0fffbbc7141f7ea65a
Prashanth Padimala <prashanth.padimala@xcubelabs.com>
2023-02-02 12:39:47 +0530

    [Task] added thanks

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <h1>Enter your name </h1>
       <input onChange={(e) => setYourName(e.target.value)} placeholder={"Enter name"} />
       {yourName && <h2>Hi {yourName}</h2>}
+      <h3>Thanks for using </h3>
     </div>
   );
 }
