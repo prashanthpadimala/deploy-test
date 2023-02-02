bda1274f8cc612b88197351a6903373790258ffb
Prashanth Padimala <prashanth.padimala@xcubelabs.com>
2023-02-02 12:13:56 +0530

    [Task] first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
 function App() {
+  const [yourName, setYourName] = React.useState("")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Enter your name </h1>
+      <input onChange={(e) => setYourName(e.target.value)} placeholder={"Enter name"} />
+      {yourName && <h2>Hi {yourName}</h2>}
     </div>
   );
 }
