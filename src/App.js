5a368a79c2354aad636d9b0bc2be6bc5405963c3
Prashanth Padimala <prashanth.padimala@xcubelabs.com>
2023-02-14 15:39:04 +0530

    [Tasl] password

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
 
   const msg = new SpeechSynthesisUtterance();
   const [value, setValue] = useState("");
+  const [password, setPassword] = useState("Xcube@123");
+  const [userPassword, setUserPassword] = useState("")
   const speechHandler = () => {
     msg.text = value;
     window.speechSynthesis.speak(msg);
@@ -17,7 +19,8 @@ function App() {
       <input onChange={(e) => setYourName(e.target.value)} placeholder={"Enter name"} />
       {yourName && <h2>Hi {yourName}</h2>}
       <h3>Thanks for using our app </h3>
-
+      {password === userPassword && <h1>iam sending my wishes</h1>}
+      <input placeholder='Enter password to see text' type={"password"} onChange={(e) => { setUserPassword(e.target.value) }} />
       <h1>Text To SPeech</h1>
       <input value={value} onChange={(e) => setValue(e.target.value)} placeholder={"Enter Text to Speak"} />
       <button variant='contained' color="secondary" onClick={speechHandler}>Speak</button>
